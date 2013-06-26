d5e9369bcc432c9fa92a282f7f765889a2095c69
imbugs <imbugs@126.com>
2013-06-26 19:09:00 +0800

    drop site by ftp

---
+++ vps/dropsite.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+cat <<EOF
+
+**************************
+* DROP SITE VERSION 1.0  *
+**************************
+
+EOF
+
+if [ "$5" == "" ];then
+  echo "usage: $0 site user passwd ldir rdir"
+  exit
+fi
+
+SITE=$1
+USER=$2
+PASSWD=$3
+LDIR=$4
+RDIR=$5
+
+echo "PARAM LIST
+==============
+SITE=$SITE
+USER=$USER
+LDIR=$LDIR
+RDIR=$RDIR
+"
+
+echo "STEP[1] : prepare to drop site $SITE"
+
+rm -rf $LDIR
+mkdir -p $LDIR
+cd $LDIR
+
+echo "STEP[2] : begin drop"
+ftp -n<<EOF
+open $SITE
+user $USER $PASSWD
+binary
+cd  $RDIR
+lcd $LDIR
+prompt
+mget *
+close
+bye
+EOF
+
+echo "STEP[3]: finished $?"
+echo ""
+echo "COPIED FILE LIST"
+echo "======================="
+ls -al $LDIR
